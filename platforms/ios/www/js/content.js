9c9e5ab5aff83c73b2bf7938c02ca05c42ab2a7a
Evan Goode <mail@evangoo.de>
2014-11-28 16:19:15 -0500

    Merge branch 'master' of https://github.com/Evan-Goode/3ten
    Conflicts:
    
    platforms/ios/3ten.xcodeproj/project.xcworkspace/xcshareddata/3ten.xcche
    ckout
    	platforms/ios/www/js/content.js
    	www/js/content.js

--- platforms/ios/www/js/content.js
+++ platforms/ios/www/js/content.js
@@ -15,7 +15,11 @@ $(document).ready(function() {
 					var content = data.posts[i].content;
 					var contentPlain = content.replace(/\<[^>]*\>/g, '');
 					$(".content-card-content-description:eq("+i+")").html(contentPlain);
+<<<<<<< HEAD
 					//$(".content-card-image:eq("+i+")").css({'background-image':'url('+data.posts[i].attachments.images.large+')'});
+=======
+>>>>>>> FETCH_HEAD
+					$(".content-card-image:eq("+i+")").css({'background-image':'url('+data.posts[i].thumbnail_images.full.url+')'});
 					//var contentImg = content.replace(regex that removes all tags except img);
 					$(".article-content:eq("+i+")").html(contentPlain);
 				}
